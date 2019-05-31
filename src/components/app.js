8de5a28d8e3fc1cf004c17d031bd8d7579b176fa
AndrewMPFQ <andrewf9001@gmail.com>
2019-05-31 14:09:29 -0600

    Andrew initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import NavigationBar from "./navbar";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <NavigationBar />
       </div>
     );
   }
---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import Logo from "../../static/assets/images/grizzlymkv_norm.png";
+
+const NavigationBar = () => {
+  return (
+    <div className="nav-wrapper">
+      <div className="left">
+        <img src={Logo} alt="Logo" />
+      </div>
+
+      <div className="center">
+        <a className="nav-link" href="#">
+          Home
+        </a>
+        <a className="nav-link" href="#">
+          Users
+        </a>
+        <a className="nav-link" href="#">
+          About
+        </a>
+        <a className="nav-link" href="#">
+          Contact
+        </a>
+      </div>
+
+      <div className="right">Chunky Monkey</div>
+    </div>
+  );
+};
+
+export default NavigationBar;
