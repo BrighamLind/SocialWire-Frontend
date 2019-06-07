55213411c5192730c141d0187f4bc0d6aa857a4d
tattooedsheep <tattooedsheep@gmail.com>
2019-06-07 11:10:52 -0600

    i forgot to commit earlier

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h2>React Redux Router</h2>
         <NavigationBar />
         <Profiles />
         <FooterNavBar />
